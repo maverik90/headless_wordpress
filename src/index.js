dcdcea6e3ed0f944e9ccadbbaafae0dd77a7a0b9
maverik90 <maverik_90@hotmail.com>
2022-03-23 18:30:16 +0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import reactlogo from "../src/react-logo.png";
+
+/**
+Challenge: find out what happens if we try to append JSX
+to our div#root using .append() instead of ReactDOM
+
+1. Create a sample page in JSX (≥ 4 elements) and save them in a variable
+2. Select the div with the ID of "root" and use `.append()` to append
+   your JSX
+3. See if you can guess what will show up in the browser before running
+   the code
+4. See if you can explain what actually shows up in the browser
+ */
+
+const page = (
+    <div>
+        <img src={reactlogo} width="40px" />
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+)
+
+
+ReactDOM.render(
+    page,
+    document.getElementById("root")
+)
